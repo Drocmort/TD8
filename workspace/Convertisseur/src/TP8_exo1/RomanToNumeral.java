c99dc2a7a0c6246dcace647c9889359f67a5bd3e
Arthur <arthur.zaccaria@gmail.com>
2016-05-10 14:35:06 +0200

    parfait

---
+++ workspace/Convertisseur/src/TP8_exo1/RomanToNumeral.java
@@ -0,0 +1,17 @@
+package TP8_exo1;
+
+public class RomanToNumeral {
+
+		private static int I=1;
+		private static int V=5;
+		private static int X=10;
+		private static int L=50;
+		private static int C=100;
+		private static int D=500;
+		private static int M=1000;
+		
+	public static void main(String[] args) {
+		
+	}
+
+}
